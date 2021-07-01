c9bc1b39f0e096af8a6295d2c67c1221ea760b70
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-06-30 23:35:47 -0500

    Added Classes project

---
+++ Classes/classes.py
@@ -0,0 +1,15 @@
+#Create Pet class
+class Pet:
+    name = ""
+    sex = ""
+    age = 0
+
+#Create new Dog class inherited from Pet class
+class Dog(Pet):
+    breed = ""
+    size = ""
+
+#Create new Fish class inherited from Pet class
+class Fish(Pet):
+    species = ""
+    water = ""
